fe3cda66ee142463b65ac54c99c57411fbb9cc87
mbergbauer1 <mbergbauer1@gmail.com>
2017-01-15 08:39:06 -0600

    n/a

--- Composer.py
+++ Composer.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import string
 from keras.models import Sequential
 from keras.layers import Dense
@@ -9,16 +9,38 @@ from keras.utils import np_utils
 
 file = "C:\\Users\\mbergbauer\\Desktop\\NN\\KernFiles\\chopin_mod.krn"
 
-split_data = []
+raw = []
 voc_map = {}
-n = 0
+nvoc = 0
+nsamp = 0
 for line in open(file):
+    nsamp += 1
     line = line.rstrip('\n')
-    split_data.append(line.split('\t'))
+    raw.append(line.split('\t'))
     for token in line.split('\t'):
         if not token in voc_map:
-            voc_map.update({token: n})
-            n += 1
+            voc_map.update({token: nvoc})
+            nvoc += 1
+
+encoded = []
+
+maxa = 0
+for i in range(len(raw)):
+    a = 0
+    tmp = []
+    for token in raw[i]:
+        tmp.append(voc_map.get(token))
+        a = a + 1
+    if a > maxa:
+        maxa = a
+    encoded.append(tmp)
+
+padded = np.empty((nsamp, maxa))
+for row in encoded:
+    tmp = np.array(np.pad(row, maxa))
+    np.append(padded, tmp)
+
+
 
 
 pass
